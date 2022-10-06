fc8735f05c5d1257b8b5d316c0607d81fa5bd32e
Diego Arturo Enriquez Mercado <93006286+Digiko2003@users.noreply.github.com>
2022-10-06 07:55:00 -0500

    06/10/2022

---
+++ src/InterfazGrafica/PruebaCampoTexto.java
@@ -0,0 +1,12 @@
+package InterfazGrafica;
+
+import javax.swing.JFrame;
+
+public class PruebaCampoTexto {
+    public static void main(String[] args) {
+        CampoTextoMarco campoTextoMarco = new CampoTextoMarco();
+        campoTextoMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        campoTextoMarco.setSize(350, 100);
+        campoTextoMarco.setVisible(true);
+    }
+} // fin de la clase PruebaCampoTexto
